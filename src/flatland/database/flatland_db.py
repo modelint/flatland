a8eded3272a7b44f9a1b810b803366cc1f76d645
Leon Starr <leon_starr@modelint.com>
2024-11-15 07:00:45 -0800

    Commented and cleaned up flatland_db.py

--- src/flatland/database/flatland_db.py
+++ src/flatland/database/flatland_db.py
@@ -2,14 +2,10 @@
 
 # System
 from collections import namedtuple
-from pathlib import Path
-from typing import NamedTuple
 
 # Model Integration
 from pyral.database import Database
-from pyral.rtypes import Attribute, Mult as DBMult
 from pyral.relvar import Relvar
-from mi_config.config import Config
 
 # Flatland
 from flatland.database.relvars import FlatlandSchema, SimpleAssoc, AssocRel, GenRel
@@ -21,45 +17,36 @@ SheetInstance = namedtuple('SheetInstance', 'standard height width size_group')
 
 class FlatlandDB:
     """
-
+    This class manages initialization and population of the Flatland database.
     """
-    db_name = 'flatland'
-    db = None
+    db_name = 'flatland'  # Text name of the database
+    db_id = None  # ID returned on successful db session initialization
     relvar_names = None
     rel_names = None
 
     @classmethod
-    def pop_sheet(cls):
+    def load_schema(cls):
         """
-        Populate the Sheet Size Group and Sheet classes
-
-        :param sheets:
-        :return:
+        Create all relvars and constraints as directed by the Flatland class models
         """
-        pass
-
-    @classmethod
-    def create_db(cls):
-        """
-
-        """
-        # Create a TclRAL session with an empty tclral
-        cls.db= Database.open_session(name='flatland')
-
         # Create all the relvars
         for subsys_name, subsys_relvars in FlatlandSchema.relvars.items():
             for relvar_name, header in subsys_relvars.items():
                 Relvar.create_relvar(db=cls.db_name, name=relvar_name,
                                      attrs=header.attrs, ids=header.ids)
-        cls.relvar_names = Database.names(db='flatland')
 
-        # Create all the rels
+        # For diagnostics we confirm by obtaining a list of all relvars the database has created
+        cls.relvar_names = Database.names(db=cls.db_name)
+
+        # Create all the relationships
         for subsys_name, rels in FlatlandSchema.rels.items():
             for r in rels:
+                # Create all simple association relationships
                 if isinstance(r, SimpleAssoc):
                     Relvar.create_association(db=cls.db_name, name=r.name,
                                               from_relvar=r.from_class, from_mult=r.from_mult, from_attrs=r.from_attrs,
                                               to_relvar=r.to_class, to_mult=r.to_mult, to_attrs=r.to_attrs)
+                # Create all associative relationships
                 if isinstance(r, AssocRel):
                     Relvar.create_correlation(db=cls.db_name, name=r.name, correlation_relvar=r.assoc_class,
                                               correl_a_attrs=r.a_ref.from_attrs, a_mult=r.a_ref.mult,
@@ -67,11 +54,36 @@ class FlatlandDB:
                                               correl_b_attrs=r.b_ref.from_attrs, b_mult=r.b_ref.mult,
                                               b_relvar=r.b_ref.to_class, b_ref_attrs=r.b_ref.to_attrs,
                                               )
+                # Create all generalization relationships
                 if isinstance(r, GenRel):
                     Relvar.create_partition(db=cls.db_name, name=r.name, superclass_name=r.superclass,
                                             super_attrs=r.superattrs, subs=r.subrefs)
 
-        cls.rel_names = Database.constraint_names(db='flatland')
+        # For diagnostics we confirm by obtaining a list of all constraints the database has created
+        cls.rel_names = Database.constraint_names(db=cls.db_name)
+
+    @classmethod
+    def create_db(cls):
+        """
+         1. Initialize a PyRAL session.
+         2. Load the Flatland Schema based on the class models defined in the project wiki (with model diagrams
+            in the documentation folder of this repository).
+         3. Populate each modeled subsystem using any yaml files in the users configuration path.
+        """
+        # Create a new PyRAL session
+        cls.db= Database.open_session(name='flatland')
+
+        # There should be no reason to create a new database unless the user has updated any of their configuration
+        # files. So, in the future, there will be an option to just load an existing populated database and move on.
+
+        # During early development, however, it is convenient to start with a fresh database build each time.
+        # So that's what we're doing for now
+
+        # Load the database schema
+        cls.load_schema()
+
+        # Populate each subsystem
+
 
         # Load sheet population
         SheetSubsysDB.pop_metadata()
